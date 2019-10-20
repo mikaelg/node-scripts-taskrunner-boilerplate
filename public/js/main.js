499bc12674b29a391d6a2dd7277045ce4200a153
Mikael Gyselinck <mikael.gyselinck@communicatiehuis.be>
2019-10-20 22:05:02 +0200

    start open navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
     
-    let  nav_icon;
+    let navigation;
+    let nav_icon;
 
+    navigation = document.getElementById('navigation');
     nav_icon = document.getElementById('nav-icon');
     nav_icon.addEventListener('click', _toggle);
     
     function _toggle() {
         nav_icon.classList.toggle('open');
+        navigation.classList.toggle('open');
     }
 
 });
