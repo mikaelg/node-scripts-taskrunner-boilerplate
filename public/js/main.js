1efebc1f63c0ecaa663a2e28730fd8bc8f210480
Mikael Gyselinck <mikael.gyselinck@communicatiehuis.be>
2019-10-21 21:41:52 +0200

    update nav and restructure design system

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     
     let navigation;
     let nav_icon;
+    let nav_icon_container;
 
     navigation = document.getElementById('navigation');
+    nav_icon_container = document.getElementById('nav-icon__container');
     nav_icon = document.getElementById('nav-icon');
     nav_icon.addEventListener('click', _toggle);
     
     function _toggle() {
         nav_icon.classList.toggle('open');
+        nav_icon_container.classList.toggle('fixed');
         navigation.classList.toggle('open');
     }
 
