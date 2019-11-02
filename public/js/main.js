cd794aed110c624656b27ecb3e38efd0ea9dc247
Mikael Gyselinck <mikael.gyselinck@communicatiehuis.be>
2019-11-02 19:52:18 +0100

    javascript html partials

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,45 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    
+
+    partials("MIG-header");
+    partials("MIG-navigation",navbutton);
+    partials("MIG-footer");
+
+});
+
+const  partials = (partial, addfunction=null) => {
+    let path = "/partials/";
+    let extention = ".html";
+    let data_atribute = "div[data='MIG']";
+    let res_data_atribute = data_atribute.replace("MIG", partial);
+    let file = document.querySelector(res_data_atribute);
+
+    if (file != null) {
+
+        fetch(path.concat(partial, extention))
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            file.innerHTML = data;  
+        })
+        .then(() => {
+            if(addfunction !=null) { addfunction(); }
+        }
+            
+        );     
+    }
+ 
+}
+
+const navbutton = () => {
     let navigation;
     let nav_icon;
     let nav_icon_container;
 
     navigation = document.getElementById('navigation');
+    console.log(document.getElementById('navigation'));
+    
     nav_icon_container = document.getElementById('nav-icon__container');
     nav_icon = document.getElementById('nav-icon');
     nav_icon.addEventListener('click', _toggle);
@@ -14,6 +48,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         nav_icon.classList.toggle('open');
         nav_icon_container.classList.toggle('fixed');
         navigation.classList.toggle('open');
-    }
+    } 
+}
 
-});
